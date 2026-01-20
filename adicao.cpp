5a116d683341e6accd03d2a78419686523fb84f4
João Pedro Palomares <joaopedropalomaresjpjp@gmail.com>
2026-01-20 14:00:05 -0300

    adicionando um arquivo que faz uma simples conta de adição

---
+++ adicao.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n1, n2, resultado;
+
+    cout << "Digite o primeiro numero: ";
+    cin >> n1;
+
+    cout << "Digite o segundo numero: ";
+    cin >> n2;
+
+    resultado = n1 + n2;
+
+    cout << resultado << endl;
+
+    return 0;
+}
